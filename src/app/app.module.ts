ea15ac70436a006c2f571af864bd9298182201f8
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-24 11:45:11 -0300

    Projeto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Module1Module } from './module1/module1.module';
+import { Module2Module } from './module2/module2.module';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    Module1Module,
+    Module2Module,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/module2/module2.module.ts
+++ src/app/module2/module2.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [Component3Component, Component4Component],
+  exports: [
+    Component3Component,
+    Component4Component
+  ],
+    imports: [
+        CommonModule
+    ]
 })
 export class Module2Module { }
